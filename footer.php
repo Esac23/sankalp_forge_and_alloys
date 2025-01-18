0585119bc78375bd5f43442e8a1a9f17492f7c53
Esac23 <118606255+Esac23@users.noreply.github.com>
2025-01-18 16:26:18 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,72 @@
+<div class="footer footer-2 ptb-100 bg-000000">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="footer-logo-area"><a href="index.php"><img src="assets/images/white-logo.png"
+                                alt="image"></a>
+                        <p>Sankalp Forge And Alloys is a leading manufacturer and trader specializing in high-quality copper fittings, cupro-nickel fittings, and medical equipment. With a commitment to precision engineering and top-tier quality.</p>
+                        <div class="footer-social-icons"><span>Follow Us:</span>
+                            <ul>
+                                <li><a href="https://www.facebook.com/" target="_blank"><i
+                                            class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                                </li>
+                                <li><a href="https://www.instagram.com/" target="_blank"><i
+                                            class="fab fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="footer-links footer-contact-list">
+                        <h3>Get In Touch</h3>
+                        <div class="footer-content-card"><i class="fas fa-map-marker-alt"></i>
+                            <h5>Location:</h5>
+                            <p><a href="#">Gala No. 06, S. No. 151, Jinal Industrial, Maharashtra, India - 401208</a>
+                            </p>
+                        </div>
+                        <div class="footer-content-card"><i class="fas fa-envelope"></i>
+                            <h5>Email:</h5>
+                            <p><a
+                                    href="mailto:sankalpforgefitting@gmail.com;"><span>sankalpforgefitting@gmail.com</span></a>
+                            </p>
+                        </div>
+                        <div class="footer-content-card"><i class="fas fa-mobile-alt"></i>
+                            <h5>Phone:</h5>
+                            <p><a href="tel:+916377726478">+91 6377726478</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="footer-links footer-quick-links">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li><a href="index.php" target="_blank"><i
+                                        class="fas fa-angle-right"></i><span>Home</span></a></li>
+                            <li><a href="about.php" target="_blank"><i class="fas fa-angle-right"></i><span>About
+                                        Us</span></a></li>
+                            <li><a href="#" target="_blank"><i
+                                        class="fas fa-angle-right"></i><span>Products</span></a></li>
+                            <li><a href="gallery.php" target="_blank"><i
+                                        class="fas fa-angle-right"></i><span>Gallery</span></a></li>
+                            <li><a href="#" target="_blank"><i
+                                        class="fas fa-angle-right"></i><span>Brochure</span></a></li>
+                            <li><a href="contact.php" target="_blank"><i class="fas fa-angle-right"></i><span>Contact
+                                        Us</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="footer-links footer-newsletter">
+                        <h3>Subscribe</h3>
+                        <p>Subscribe To Our Newsletter To Get Our Update News!</p>
+                        <div class="input-group"><input type="text" class="form-control"
+                                placeholder="Your Email"><button class="btn" type="button"><i
+                                    class="far fa-paper-plane"></i></button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ header.php
@@ -0,0 +1,87 @@
+<div class="theme-switcher"><label id="switch" class="switch"><input type="checkbox" onchange="toggleTheme()"
+            id="slider"><span class="slider round"></span></label></div>
+<div class="topbar topbar-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="topbar-left-side">
+                    <ul>
+                        <li><i class="fas fa-phone-alt"></i><a href="tel:+6377726478">+91 6377726478</a></li>
+                        <li><i class="fas fa-envelope"></i><a
+                                href="mailto:sankalpforgefitting@gmail.com;">sankalpforgefitting@gmail.com</a></li>
+                        <li><i class="fas fa-map-marker-alt"></i><a href="">Vasai East, Palghar 401208, Maharashtra,
+                                India</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="topbar-right-side">
+                    <div class="header-social-icons">
+                        <ul>
+                            <li><a href="https://www.facebook.com/" target="_blank"><i
+                                        class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://www.linkedin.com/" target="_blank"><i
+                                        class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li><a href="https://www.instagram.com/" target="_blank"><i
+                                        class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="header-area header-area-2">
+    <div class="navbar-area">
+        <div class="main-responsive-nav">
+            <div class="container">
+                <div class="mobile-nav"><a href="index.php" class="logo"><img class="black-logo"
+                            src="assets/images/logo.png" alt="logo"> <img class="white-logo"
+                            src="assets/images/white-logo.png" alt="logo"></a>
+                    <div class="menu-sidebar-1 menu-small-device">
+                        <div class="call-button"><i class="fas fa-headset"></i>
+                            <p>Have Any<span> Questions?</span></p><a href="tel:+6377726478">+91 6377726478</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light"><a class="navbar-brand" href="index.php"><img
+                            class="black-logo" src="assets/images/logo.png" alt="logo"> <img class="white-logo"
+                            src="assets/images/white-logo.png" alt="logo"></a>
+                    <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                            <li class="nav-item plus-icon"><a href="index.php"
+                                    class="nav-link dropdown-toggle active">Home</a></li>
+                            <li class="nav-item plus-icon"><a href="about.php" class="nav-link dropdown-toggle">About Us</a>
+                            </li>
+                            <li class="nav-item"><a href="#" class="nav-link dropdown-toggle">Our Products<i
+                                        class="fas fa-angle-down"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a href="copper-pipe-u-bend.php" >Copper Pipe U Bend<i
+                                                ></i></a>
+
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a href="gallery.php" class="nav-link">Gallery</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Brochure</a></li>
+                            <li class="nav-item"><a href="contact.php" class="nav-link">Contact Us</a></li>
+                        </ul>
+                        <div class="menu-sidebar menu-sidebar-1"><a class="default-button-3 default-button-3-h"
+                                href="#">Get A Quote<i class="fas fa-long-arrow-alt-right"></i></a>
+                            <div class="call-button"><i class="fas fa-headset"></i>
+                                <p>Have Any<span> Questions?</span></p><a href="tel:+916377726478">+91
+                                    6377726478</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
